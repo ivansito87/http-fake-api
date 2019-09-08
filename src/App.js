c02152b71bb3656749f3b4c376f4338e2f9fbbe9
Ivan Rendon <ivansito87@gmail.com>
2019-09-08 17:07:55 -0400

    adds toast and service for logging errors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 // language=JSX
 import React, { Component } from "react";
+
+//=========== Toast =================================================
+/*
+* Toast is helping us to display errors and success we can use the colors provided
+* by Bootstrap like success and warning, info and danger
+* we can use it like this :TODO toast.success and toast to get all the colors
+* */
+import { ToastContainer } from "react-toastify";
+import { toast } from "react-toastify";
+//=========== Toast =================================================
+
+//=========== Sentry =================================================
+// we can install this package for our application in this
+// https://sentry.io/j but
+import * as Sentry from "@sentry/browser";
+//=========== Sentry =================================================
+
+
+import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
 import "./App.css";
-
-const apiEndPoint = "https://jsonplaceholder.typicode.com/posts";
+import config from "../src/config";
 class App extends Component {
   state = {
     posts: []
@@ -15,7 +33,7 @@ class App extends Component {
       body: "b"
     };
 
-    axios.post(apiEndPoint, obj)
+    axios.post(config.apiEndPoint, obj)
       .then(res => {
         const { data } = res;
         const posts = [data, ...this.state.posts];
@@ -31,19 +49,21 @@ class App extends Component {
   // maintained
   async componentDidMount() {
     // pending > resolved (success) OR rejected > (failure)
-    const {data: posts} = await axios.get(apiEndPoint);
+    const {data: posts} = await axios.get(config.apiEndPoint);
     this.setState({ posts })
 
   }
 
   handleUpdate = post => {
     post.title = "UPDATED";
-    axios.put(`${apiEndPoint}/${post.id}`, post)
+    axios.put(`${config.apiEndPoint}/${post.id}`, post)
       .then(res => {
         const posts = [...this.state.posts];
         const index = posts.indexOf(post);
         posts[index] = {...post};
         this.setState({posts});
+        toast.success("Updating item");
+        toast("Updating item");
       })
       .catch(err => err ? console.log(err.message) : "" );
 
@@ -54,7 +74,7 @@ class App extends Component {
     // we need to store the data in a variable to use it later
     const originalPosts = this.state.posts;
 
-    axios.delete(`${apiEndPoint}/${post.id}`)
+    axios.delete(`s${config.apiEndPoint}/${post.id}`)
       .then(r => {
         // We will use this line of code below to simulate that there was an error
         // while requesting the delete method,
@@ -79,7 +99,14 @@ class App extends Component {
         if (err) {
           alert("There was an error while deleting the post");
           this.setState({ posts: originalPosts });
-          console.log(err.message);
+
+          /*
+          * Sentry is a powerful Service that creates issues for you applications since when we are in production we
+          * dont have access to the console.log we can see the issues coming from the users computer as they are using
+          * our application  the usage is very simple and easy to install.
+          *
+          * */
+          Sentry.captureException(err);
         }
 
 
@@ -90,6 +117,7 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
+      <ToastContainer />
         <button className="btn btn-primary" onClick={this.handleAdd}>
           Add
         </button>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
+import * as Sentry from "@sentry/browser";
 
+Sentry.init({ dsn: "https://ef9eadad234a407f82ab49e1649c6824@sentry.io/1660738" });
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
